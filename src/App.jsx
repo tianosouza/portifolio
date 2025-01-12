b800bd24919e9fe73989c0617f5dab12768abe7a
Adriano Skilo <ltianosouzal@outlook.com>
2025-01-11 23:04:55 -0300

    update app

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
+import Footer from './components/footer';
 import Header from './components/header';
-import ThemeToggle from './components/toggle';
 import './Global.css';
 
 export function App() {
   return (
-    <div className="min-h-screen bg-gray-100 text-gray-900 dark:bg-gray-900 dark:text-gray-100">
+    <div className="min-h-screen bg-gray-100 text-gray-900 dark:bg-gray-800 dark:text-gray-100">
       <Header />
       <main className="container mx-auto p-4">
         
       </main>
+      <Footer />
     </div>
 
   );
