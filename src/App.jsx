5ecd5125da3589cb183ece1e2071da9b1e18cbc7
Adriano Skilo <ltianosouzal@outlook.com>
2025-01-11 22:25:32 -0300

    modify App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-import './Global.css'
+import Header from './components/header';
+import ThemeToggle from './components/toggle';
+import './Global.css';
 
 export function App() {
   return (
-    <>
-      
-    </>
-  )
+    <div className="min-h-screen bg-gray-100 text-gray-900 dark:bg-gray-900 dark:text-gray-100">
+      <Header />
+      <main className="container mx-auto p-4">
+        
+      </main>
+    </div>
+
+  );
 }
