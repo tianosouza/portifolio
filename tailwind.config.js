1e85ea4ff2d62c35cd2edfd5e0533c4efd7ca7c0
Adriano Skilo <ltianosouzal@outlook.com>
2025-01-18 16:05:40 -0300

    add new color palet

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,25 @@ export default {
   darkMode: 'class',
   content: ["./src/**/*.{html,js,ts,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'color-primary': {
+          DEFAULT: '#001C55',
+          light: '#001C55',
+          dark: '#00072D',
+        },
+        'color-secondary': {
+          DEFAULT: '#0A2472',
+          light: '#0E6BA8',
+          dark: '#0A2472',
+        },
+        'color-text': {
+          DEFAULT: '#A6E1FA',
+          light: '#A6E1FA',
+          dark: '#0E6BA8',
+        },
+      }
+    },
   },
   plugins: [],
 }
